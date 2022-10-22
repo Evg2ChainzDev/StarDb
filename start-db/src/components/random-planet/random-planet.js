bf9dc412b605144c78e3b01c4432cc158ed719be
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2022-10-22 23:21:36 +0300

    7-4

--- start-db/src/components/random-planet/random-planet.js
+++ start-db/src/components/random-planet/random-planet.js
@@ -22,6 +22,10 @@ export default class RandomPlanet extends Component {
     // clearInterval(this.interval);
   }
 
+  componentDidMount() {
+    console.log(' componentDidMount() ')
+  }
+
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
